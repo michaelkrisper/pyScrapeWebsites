058e61524eafa723014f35442d0d3b1a12c08553
Michael Krisper <michael.krisper@gmail.com>
2015-02-03 15:16:32 +0100

    Initial commit

---
+++ scrape.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# coding=utf-8
+"""
+
+"""
+
+import requests
+
+__author__ = "Michael Krisper"
+__email__ = "michael.krisper@gmail.com"
+__date__ = "2015-mm-dd"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
